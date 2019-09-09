23344fba71f2739da61ef030a6dcb881a69f5bdd
Phuc <tanphuc16707@gmail.com>
2019-09-09 11:09:17 +0700

    Change Output Path

--- Text2IMG/data_aumentation.py
+++ Text2IMG/data_aumentation.py
@@ -14,6 +14,7 @@ from numpy import expand_dims
 from matplotlib import pyplot
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import ImageDataGenerator
+
 #read data from docx and get words
 def getTokens(doc_path):
     chars = ['“','”','"','.',', ',':','?','>','<',';',':','[',']','{','}',"''",') ',' (','! ','- ','_ ']
@@ -39,6 +40,7 @@ def getTokens(doc_path):
                         for word in sequence.split(' '):
                             word = re.sub(' +', ' ', word)
                             word = word.strip()
+                            #'T2 ís china text'
                             if t == 'T1':
                                 if len(word) == 0:
                                     pass
@@ -47,6 +49,7 @@ def getTokens(doc_path):
                                     token_index[word] = len(token_index) + 1
                                     index_token[len(token_index)] = word
                             else:
+                                # each char in china as word
                                 for char in word:
                                     if len(char) == 0:
                                         pass
@@ -70,18 +73,23 @@ def randSeq(index_token,nb_sequence):
         random.shuffle(key_list)
         random.shuffle(nb_words)
         for idx in range(nb_words[0]):
-            if index_token[key_list[idx]].islower():
-                width += int(len(index_token[key_list[idx]])*1.2)
-            else:
-                width += int(len(index_token[key_list[idx]])*1.5)
-            sequence += ' '+index_token[key_list[idx]]
+            #sequence += ' '+index_token[key_list[idx]]
+            sequence += ' '+index_token[key_list[i]]
         sequences.append(sequence)
         w_seq.append(width)
-    return [sequences,w_seq]
+    return sequences
 
-def text2IMG(sequences,w_seq):
+def draw_underlined_text(draw, pos, text, font, under, **options):
+    #if under = 1, draw underline
+    twidth, theight = draw.textsize(text, font=font)
+    lx, ly = pos[0], pos[1] + theight
+    draw.text(pos, text, font=font, **options)
+    if under == 1:
+        draw.line((lx+7, ly+1, lx + twidth, ly+1), **options)
+        
+def text2IMG(sequences):
     bg_path = './Background/'
-    save_path = './Generate2/'
+    save_path = '../Data/'
     font_path = './Fonts/VN/'
     colors = {
                 0:(0, 0, 0),
@@ -90,7 +98,7 @@ def text2IMG(sequences,w_seq):
     seq_idx = 0
     f_size = 25
     count = 0
-    nb_gen = 3
+    nb_gen = 2
     datagen = ImageDataGenerator(
             zca_whitening=True, featurewise_center=True,featurewise_std_normalization=True,
             rotation_range=0.8, zoom_range=[0.95, 1.05], brightness_range=[0.2,1.0],
@@ -105,18 +113,16 @@ def text2IMG(sequences,w_seq):
             for font in os.listdir(font_path):
                 f_without_ext = os.path.splitext(font)[0]
                 img1 = img.copy()
-                d = ImageDraw.Draw(img1)
+                draw = ImageDraw.Draw(img1)
                 fnt = ImageFont.truetype(font_path+font, f_size,encoding="utf-8")
-                w,h = d.textsize(sequence,font=fnt)
-                img1 =img1.resize((w,h-2))
-                d = ImageDraw.Draw(img1)
-                d.text((-7,-5),sequence, font=fnt, fill=colors[random.choice([0,1])])
-                img1 = img1.resize((124,124))
-                data = img_to_array(img1)   
+                width,height = draw.textsize(sequence,font=fnt)
+                new_img =img1.resize((width,height-2))
+                draw = ImageDraw.Draw(new_img)
+                draw_underlined_text(draw, (-7,-5), sequence, font=fnt, fill=colors[random.choice([0,1])],under=random.choice([0,1]))
+                new_img = new_img.resize((124,124))
+                data = img_to_array(new_img)   
                 # expand dimension to one sample
                 samples = expand_dims(data, 0)
-                #img1.save(save_path+image_name)
-                #writer.writerow([image_name, sequence])
                 # prepare iterator
                 it = datagen.flow(samples, batch_size=1)
                 # generate samples and plot
@@ -127,18 +133,14 @@ def text2IMG(sequences,w_seq):
                 	batch = it.next()
                 	# convert to unsigned integers for viewing
                 	image = batch[0].astype('uint8')
-                	# plot raw pixel data
                 	pyplot.imsave(save_path+str(seq_idx)+'/img-'+str(count)+'-'+bg_without_ext+'-'+f_without_ext+'.png',image)
-                #for i in range(count-nb_gen,count+1):
-                   # im_name = 'img-'+str(i)+'-'+bg_without_ext+'-'+f_without_ext+'.png'
-                    #writer.writerow([img1,im_name, sequence])
                 print ('generate image-'+ str(count)+ '.png done.')
                 count += 1
         seq_idx += 1
         
     print('done.')
 doc_path = './Text/'
-nb_sequence = 1000
+nb_sequence = 20
 token_index,index_token = getTokens(doc_path)
-sequences,w_seq = randSeq(index_token,nb_sequence)
-text2IMG(sequences,w_seq)
+sequences = randSeq(index_token,nb_sequence)
+text2IMG(sequences)
