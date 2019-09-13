21c3d03f7659d1de4262cfd44be91d31bb6aaefa
Nam <huynhthehainam@gmail.com>
2019-09-13 17:38:42 +0700

    no message

--- RunProject.py
+++ RunProject.py
@@ -3,9 +3,11 @@ import math
 import numpy as np
 import glob
 import os
-from Model import SiameseModel
+import copy
+# from Model import SiameseModel
 
 class CompareTwoTextImage:
+    ThresholdNumber = 145
     def __init__(self, ModelSiamese):
         self.Model = ModelSiamese
     def AverageOfList(self,ListNumber):
@@ -18,186 +20,109 @@ class CompareTwoTextImage:
             if ListNumber[i] < ThreshNumber:
                 ListNumber[i] = 0
         return ListNumber
+    SpreadWidth = 2
     def CompareTwoImage(self, Image1Path, Image2Path):
         Image1 = cv2.imread(Image1Path)
-
         Image2 = cv2.imread(Image2Path)
-        ListWordImage1 = self.SplitWord(Image1)
-        ListWordImage2 = self.SplitWord(Image2)
-        CounterSame = 0
-        for Index1 in range(len(ListWordImage1)):
-            IsSame = False
-            for RangeIndex in range(-2,3):
-                Index2 = min(max(0, Index1 + RangeIndex), len(ListWordImage2)-1)
-                Pred = self.Model.PredictOnePairImage(ListWordImage1[Index1],ListWordImage2[Index2])
+        ListWordImage1 = self.SplitWordCombine(Image1,145,(4,20))
+        # ListWordImage2 = self.SplitWordCombine(Image2,145,(4,5))
+        # CounterSame = 0
+        # IndexNext = 0
+        # for Index1 in range(len(ListWordImage1)):
+        #     IsSame = False
+        #     for RangeIndex in range(-2,3):
+        #         IndexNext = Index1
+        #         Index2 = min(max(0, IndexNext + RangeIndex), len(ListWordImage2)-1)
+        #         Pred = self.Model.PredictOnePairImage(ListWordImage1[Index1],ListWordImage2[Index2])
+        #         if Pred[0][0] >0.5:
+        #             IsSame = True
+        #             IndexNext = Index2 +1
+        #     if IsSame:
+        #         CounterSame+=1
                 
-                if Pred[0][0] >0.7:
-                    IsSame = True
-            if IsSame:
-                CounterSame+=1
-            else:
-                for RangeIndex in range(-2,3):
-                    Index2 = min(max(0, Index1 + RangeIndex), len(ListWordImage2)-1)
-                    ConcatImage = np.concatenate([ListWordImage1[Index1],ListWordImage2[Index2]], axis=1)
-                    cv2.imshow('{}'.format(Pred[0][0]),ConcatImage)
-                    cv2.waitKey(0)
-                    cv2.destroyAllWindows()
-        return CounterSame/len(ListWordImage1)*100
-    def SplitWord(self, Image):
+
+        #     else:
+        #         for RangeIndex in range(-2,3):
+        #             IndexNext = Index1
+        #             Index2 = min(max(0, IndexNext + RangeIndex), len(ListWordImage2)-1)
+        #             Pred = self.Model.PredictOnePairImage(ListWordImage1[Index1],ListWordImage2[Index2])
+        #             ConcatImage = np.concatenate([ListWordImage1[Index1],ListWordImage2[Index2]], axis=1)
+        #             cv2.imshow('{}'.format(Pred[0][0]),ConcatImage)
+        #             cv2.waitKey(0)
+        #             cv2.destroyAllWindows()
+        # return CounterSame/len(ListWordImage1)*100
+        return True
+    def SplitLine(self,Img, ThresholdNumber,Kernel):
+        T = ThresholdNumber
+        Gray  = cv2.cvtColor(src = Img,code= cv2.COLOR_BGR2GRAY)
+        (T, Binary) = cv2.threshold(src = Gray,thresh=T,maxval=255,type=cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)
+        RectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, Kernel)
+        Dilation = cv2.dilate(Binary, RectKernel, iterations =1)
+        Contours, hierarchy = cv2.findContours(image= Dilation, mode=cv2.RETR_TREE,method=cv2.CHAIN_APPROX_SIMPLE)
+        BoundingContours = []
+        for Contour in Contours:
+            x,y,w,h = cv2.boundingRect(Contour)
+            BoxContour = {'x':x,'y':y,'w':w,'h':h}
+            BoundingContours.append(BoxContour)
+
+        BoundingContours = sorted(BoundingContours,key= lambda X: X['y'])
+        SplittedLines =[]
+        for Index, BoxContour in enumerate(BoundingContours):
+            ImgWrite = Img[BoxContour['y']:(BoxContour['y'] + BoxContour['h']),BoxContour['x']:(BoxContour['x']+BoxContour['w'])]
+            ImgShow = Binary[BoxContour['y']:(BoxContour['y'] + BoxContour['h']),BoxContour['x']:(BoxContour['x']+BoxContour['w'])]
+            CounterWhite = 0
+            for i in range(ImgShow.shape[0]):
+                for ii in range(ImgShow.shape[1]):
+                    if ImgShow[i][ii] == 255:
+                        CounterWhite +=1
+            if CounterWhite >= 0.15 *ImgShow.shape[0] *ImgShow.shape[1] :
+                SplittedLines.append(ImgWrite)
+        return SplittedLines
+    def SplitWord(self,Img, ThresholdNumber, Kernel):
+        T = ThresholdNumber
+        Gray  = cv2.cvtColor(src = Img,code= cv2.COLOR_BGR2GRAY)
+        (T, Binary) = cv2.threshold(src = Gray,thresh=T,maxval=255,type=cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)
+        RectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, Kernel)
+        Dilation = cv2.dilate(Binary, RectKernel, iterations =1)
+
+        Contours, hierarchy = cv2.findContours(image= Dilation, mode=cv2.RETR_EXTERNAL,method=cv2.CHAIN_APPROX_TC89_L1)
+        BoundingContours = []
+        for Contour in Contours:
+            x,y,w,h = cv2.boundingRect(Contour)
+            BoxContour = {'x':x,'y':y,'w':w,'h':h}
+            BoundingContours.append(BoxContour)
+
+        BoundingContours = sorted(BoundingContours,key= lambda X: X['x'])
+        SplittedWords =[]
+        for Index, BoxContour in enumerate(BoundingContours):
+            ImgWrite = Img[BoxContour['y']:(BoxContour['y'] + BoxContour['h']),BoxContour['x']:(BoxContour['x']+BoxContour['w'])]
+            ImgShow = Binary[BoxContour['y']:(BoxContour['y'] + BoxContour['h']),BoxContour['x']:(BoxContour['x']+BoxContour['w'])]
+            # cv2.imshow('',ImgShow)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            CounterWhite = 0
+            for i in range(ImgShow.shape[0]):
+                for ii in range(ImgShow.shape[1]):
+                    if ImgShow[i][ii] == 255:
+                        CounterWhite +=1
+            if CounterWhite >= 0.15 *ImgShow.shape[0] *ImgShow.shape[1] :
+                # print(ImgShow)
+                SplittedWords.append(ImgWrite)
+        return SplittedWords
+
+    
+    def SplitWordCombine (self, Image, ThresholdNumber, Kernel):
         files = glob.glob('./Result/*')
         for f in files:
             os.remove(f)
-        ThreshHold1 = 0
-        ThreshHold2 = 0
-        Hihi=[]
-        RealImage = Image
-        grayImage = cv2.cvtColor(RealImage, cv2.COLOR_BGR2GRAY)
-        _, thresh1 = cv2.threshold(grayImage, 175, 255, cv2.THRESH_BINARY)
-        ListAmplitude = []
-        for i in range(thresh1.shape[0]):
-            Amplitude = 0
-            for ii in range(thresh1.shape[1]):
-                if thresh1[i, ii] == 0:
-                    Amplitude += 1
-            ListAmplitude.append(Amplitude)
-        ListAmplitude = self.ThesholdList(ListAmplitude,ThreshHold1)
-        CounterSpacePixel = 0
-        SpaceHeights = []
-        for i, Amplitude in enumerate(ListAmplitude):
-            if Amplitude <= 0:
-                CounterSpacePixel +=1
-            else:
-                if CounterSpacePixel >0:
-                    SpaceHeights.append(CounterSpacePixel)
-                CounterSpacePixel=0
-        # print(SpaceHeights)
-
-        CurrentAmplitude = 0
-        ListCroppedLine = []
-        ListCroppedRealLine = []
-        StartPoint = 0
-        EndPoint = 0
-        SpaceHeight = 5
-        if len(SpaceHeights)>0:
-            SpaceHeights.pop(0)
-        if len (SpaceHeights)>0:
-            SpaceHeight = math.ceil(self.MiddleOfList(SpaceHeights)*0.2)
-        IsDetectEndPoint = True
-        for i, Amplitude in enumerate(ListAmplitude):
-            if Amplitude > 0 and CurrentAmplitude <= 0 and IsDetectEndPoint:
-                StartPoint = i
-                IsDetectEndPoint = False
-            if Amplitude <= 0 and CurrentAmplitude > 0:
-                IsCoupleOfPixelABlack = False
-                for ii in range(SpaceHeight):
-                    if i+ii<len(ListAmplitude) and ListAmplitude[i+ii]>0:
-                        IsCoupleOfPixelABlack = True
-                if not IsCoupleOfPixelABlack:
-                    IsDetectEndPoint = True
-                    EndPoint = i
-                    ListCroppedLine.append(thresh1[max(StartPoint,0):min(EndPoint,thresh1.shape[0]), :])
-                    ListCroppedRealLine.append(RealImage[max(StartPoint,0):min(EndPoint,thresh1.shape[0]), :])
-            CurrentAmplitude = Amplitude
-        
+        SplittedLines = self.SplitLine(Image, ThresholdNumber,(15,3))
         ListResizedCroppedRealWord = []
-        for IndexList in range(len(ListCroppedLine)):
-            img =  ListCroppedLine[IndexList]
-            imgReal = ListCroppedRealLine[IndexList]
-            # cv2.imwrite('./Result/Line{}.png'.format(IndexList),img)
-            ListAmplitude = []
-            ListCroppedWord = []
-            ListCroppedRealWord =[]
-            MaxWidth =  -9999
-            SpaceWidths = []
-            AngleBest = 0
-            for AngleLoop in range(20):
-                ListAmplitudeInside=[]
-                Angle  = AngleLoop*math.pi/180
-                for i in range(img.shape[1]):
-                    Amplitude = 0
-                    for ii in range(img.shape[0]):
-                        # print('Ver: {}, Hor: {}'.format(ii,int(min(0,i-ii*math.sin(Angle)))))
-                        if img[ii,int(max(0,i-ii*math.sin(Angle)))] == 0:
-                            Amplitude += 1
-                    ListAmplitudeInside.append(Amplitude)
-                StartPoint = 0
-                EndPoint = 0
-                
-                BeforeAmplitude = 0
-                CounterSpacePixel = 0
-                SpaceWidthsInside = []
-                for i, Amplitude in enumerate(ListAmplitudeInside):
-                    if Amplitude <= 0:
-                        CounterSpacePixel +=1
-                    else:
-                        if CounterSpacePixel >0:
-                            SpaceWidthsInside.append(CounterSpacePixel)
-                        CounterSpacePixel=0
-                # print('Line {}: Amplitudes {}'.format(IndexList,ListAmplitude))
-                if len(SpaceWidthsInside)>0: 
-                    SpaceWidthsInside.pop(0)
-                if len(SpaceWidthsInside)>0:
-                    # print('Best Angle: {} MaxSpace: {}'.format(AngleLoop,max                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            (SpaceWidthsInside)))
-                    if len(SpaceWidthsInside) > MaxWidth:
-                        MaxWidth = len(SpaceWidthsInside)
-                        SpaceWidths = SpaceWidthsInside
-                        ListAmplitude = ListAmplitudeInside
-                        AngleBest  = AngleLoop
-            WordWidths = []
-            CounterWordPixel = 0
-            # print('Amplitudes: {}'.format(ListAmplitude))
-            for i, Amplitude in enumerate(ListAmplitude):
-                if Amplitude > 0:
-                    CounterWordPixel +=1
-                else:
-                    if CounterWordPixel >0:
-                        WordWidths.append(CounterWordPixel)
-                    CounterWordPixel=0
-            SpaceWidth = 3
-            if len(WordWidths)>0:
-                # print('Line: {} Best Angle: {} WordWidth: {}'.format(IndexList,AngleBest, WordWidths))
-                # print('MaxSpaceWidth: {} MaxWordWidth: {}'.format(max(SpaceWidths),max(WordWidths)))
-                # print('Height: {}'.format(img.shape[0]))
-                Height =  img.shape[0]
-            
-                SpaceWidth  = math.ceil(self.MiddleOfList(WordWidths)*0.3)
-            # if max(WordWidths)<Height:
-            #     SpaceWidth = self.MiddleOfList(SpaceWidths)
-            # SpaceWidth =  min(SpaceWidths)
-            # print('SpaceWidth: {}'.format(SpaceWidth))
-            # ShowAmplitudeBar(ListAmplitude)
-            IsDetectEndPoint = True
-            CurrentAmplitude = 0
-            # print(AngleBest)
-            AngleBest =  AngleBest*math.pi/180
-            ListAmplitude = self.ThesholdList(ListAmplitude,ThreshHold2)
-            for i, Amplitude in enumerate(ListAmplitude):
-                if Amplitude > 0 and CurrentAmplitude <= 0 and IsDetectEndPoint:
-                    for NextPixel in range(1,SpaceWidth):
-                        if ListAmplitude[i - NextPixel]>0 or i - NextPixel<0:
-                            break
-                    StartPoint = i - int(NextPixel/2)
-                    IsDetectEndPoint = False
-                if Amplitude <= 0 and CurrentAmplitude > 0:
-                    IsCoupleOfPixelABlack = False
-                    for ii in range(SpaceWidth):
-                        if i+ii<len(ListAmplitude) and ListAmplitude[i+ii]>0:
-                            IsCoupleOfPixelABlack = True
-                    if not IsCoupleOfPixelABlack:
-                        IsDetectEndPoint = True
-                        for NextPixel in range(SpaceWidth):
-                            if ListAmplitude[i + NextPixel]>0 or i+NextPixel>=len(ListAmplitude):
-                                break
-                        EndPoint = i + int(NextPixel/2)
-                        # print('StartPoint: {}, EndPoint: {}'.format(StartPoint,EndPoint))
-                        ListCroppedWord.append(img[:,max(StartPoint-int(img.shape[0]*math.sin(AngleBest)),0):min(EndPoint,img.shape[1])])
-                        ListCroppedRealWord.append(imgReal[:,max(StartPoint-int(img.shape[0]*math.sin(AngleBest)),0):min(EndPoint,img.shape[1])])
-                CurrentAmplitude = Amplitude
-           
-            for i, Img in enumerate(ListCroppedRealWord):
-                Hihi.append(Img.shape[1]/Img.shape[0])
-                # print('-----------{}-----------'.format(Img.shape[1]/Img.shape[0]))
-                # print('{} {}'.format(Img.shape[1],Img.shape[0]))
+        for IndexList in range(len(SplittedLines)):
+            LineImg =  SplittedLines[IndexList]
+            MinWord = -1
+            SplittedWordsOut = []
+            SplittedWordsOut = self.SplitWord(LineImg, ThresholdNumber,Kernel)
+            for i, Img in enumerate(SplittedWordsOut):
                 ResizedImage = np.zeros((max(Img.shape[0],Img.shape[1]),max(Img.shape[0],Img.shape[1]),3),np.uint8)
                 ResizedImage[:] = (255,255,255)
                 VerticalStartPoint =  int((ResizedImage.shape[0] - Img.shape[0])/2)
@@ -205,12 +130,48 @@ class CompareTwoTextImage:
                 for VerI in range(Img.shape[0]):
                     for HorI in range(Img.shape[1]):
                         ResizedImage[VerI+VerticalStartPoint,HorI + HorizonalStartPoint] = Img[VerI,HorI]
+
                 ResizedImage = cv2.resize(ResizedImage, (124,124))
                 ListResizedCroppedRealWord.append(ResizedImage)
-                # cv2.imwrite('./Result/Line{}Word{}.png'.format(IndexList,i),ResizedImage)
+        for i,Image in enumerate(ListResizedCroppedRealWord):
+            cv2.imwrite('./Result/Word{}.png'.format(i),Image)
+
+
+
+        # ListRatio = []
+        # for i, Image in enumerate(ListResizedCroppedRealWord):
+        #     Ratio = (Image.shape[1] / Image.shape[0])
+        #     if Ratio >5:
+        #         NewKernel = Kernel
+        #         AA = NewKernel[0]-1
+        #         BB =  NewKernel[1]
+        #         NewKernel = (AA,BB)
+        #         NewSplitedWord = self.SplitWord(Image,ThresholdNumber,NewKernel)
+        #         if len(NewSplitedWord)>1:
+        #             for aaa in NewSplitedWord:
+        #                 cv2.imshow('',aaa)
+        #                 cv2.waitKey(0)
+        #                 cv2.destroyAllWindows()
+        #         print(NewKernel)
+        #     ListRatio.append(Ratio)
+        #     print('Word {} ratio {}'.format(i,Ratio))
+        # print('Max: {} Index Max: {}'.format(max(ListRatio),ListRatio.index(max(ListRatio))))
+        print(len(ListResizedCroppedRealWord))
         return ListResizedCroppedRealWord
+
+    def SpreadList(self,ListNumber,SpreadWidth):
+        ListAdd =[]
+        for i in range(len(ListNumber)):
+            if ListNumber[i]>0:
+                ListAdd.append(i)
+        for Index in ListAdd:
+            for ii in range(SpreadWidth):
+                IndexFill = Index - ii
+                IndexFill = min(max(IndexFill,0), len(ListNumber)-1)
+                ListNumber[IndexFill] += 5
+        return ListNumber
 ModelSiamese = ''
-ModelSiamese = SiameseModel()
-ModelSiamese.LoadWeight('WeightProject.h5')
+# ModelSiamese = SiameseModel()
+# ModelSiamese.LoadWeight('WeightProject.h5')
 CompareCore = CompareTwoTextImage(ModelSiamese)
-print(CompareCore.CompareTwoImage('TestProject1.PNG','TestProject2.PNG'))
+print(CompareCore.CompareTwoImage('Capture.png','TestProject2.PNG'))
